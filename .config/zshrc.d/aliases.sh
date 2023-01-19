02128cee11f6b5ed63cd1ee4ee3ecbfbb784130e
Bali Harko <harkobal@spar.local>
2023-01-19 11:05:35 +0100

    update aliases

--- .config/zshrc.d/aliases.sh
+++ .config/zshrc.d/aliases.sh
@@ -1,1 +1,3 @@
 alias df-git='git --git-dir=$HOME/.cache/df-git --work-tree=$HOME'
+alias mvn8='JAVA_HOME=/usr/lib/jvm/java-1.8.0 /usr/bin/mvn'
+alias mvn11='JAVA_HOME=/usr/lib/jvm/java-11-openjdk/ mvn'
